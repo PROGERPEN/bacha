d3fe6f9eb081384d60e5513efe3b0d0a3e61b05c
a17571621 <k612nn@gmail.com>
2019-11-07 00:44:51 +0300

    зарефакторил и добавил рекламу

---
+++ app/src/main/java/close/adult/meetbit/LastRtyMireaActivity.kt
@@ -0,0 +1,12 @@
+package close.adult.meetbit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LastRtyMireaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_last_rty_mirea)
+    }
+}
